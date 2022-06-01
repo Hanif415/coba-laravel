9ef76614d2a563b937a087d1eabe0d6bef4b2c19
hanif415 <hanifamrullah42@gmail.com>
2022-06-01 14:08:56 +0700

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hanif | About</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-    <img src="img/yukiko.jpg" alt="yukiko" width="300">
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+    <img src="img/yukiko.jpg" alt="yuk" width="300">  
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hanif</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
-  <body>
+@extends('layouts.main')
 
-    <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
-        <div class="container">
-          <a class="navbar-brand" href="/">Hanif Blog</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/blog">Blog</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div class="container mt-4">
-
-          <h1>Hello, world!</h1>
-
-      </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  </body>
-</html>
+@section('container')
+    <h1>Halaman Home</h1>
+@endsection
