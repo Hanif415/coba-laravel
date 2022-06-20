4796769fbb6187561ed2bf7cc16704caae73b8fa
hanif415 <hanifamrullah42@gmail.com>
2022-06-20 13:00:57 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('/categories', function(){
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 
 Route::get('/register', [RegisterController::class, 'index']);
