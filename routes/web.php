827d062007ec77ef60a8a1b5faefe07a4fcc94af
hanif415 <hanifamrullah42@gmail.com>
2022-06-24 17:07:51 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,6 +51,7 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, function(){
+Route::get('/dashboard', function(){
         return view('dashboard.index');
-}])->middleware('auth');
+})->middleware('auth');
+Route :: resource ('/dashboard/posts ' , DashboardPostController::class)->middleware('auth') ;
